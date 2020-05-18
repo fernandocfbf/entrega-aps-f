b09259517f9b7d390328c0150ee47148dc76291f
F.Fincatti <fernandocfbf@al.insper.edu.br>
2020-05-18 19:22:02 -0300

    Fiz o requisito 1, uma porta And de três entradas, os teste passam

---
+++ src/main/java/model/And3Gate.java
@@ -0,0 +1,44 @@
+package model;
+
+public class And3Gate extends Gate{
+    private final NandGate nand1;
+    private final NandGate nand2;
+    private final NandGate nand3;
+    private final NandGate nand4;
+
+    public And3Gate() {
+        super("AND", 2);
+
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+        nand3 = new NandGate();
+        nand4 = new NandGate();
+
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+
+        nand3.connect(0, nand2);
+
+        nand4.connect(0, nand3);
+        nand4.connect(1, nand3);
+
+    }
+
+    @Override
+    public boolean read() {
+        return nand4.read();
+    }
+
+    @Override
+    public void connect(int inputIndex, Emitter emitter) {
+        if (inputIndex < 0 || inputIndex > 2) {
+            throw new IndexOutOfBoundsException(inputIndex);
+        }
+
+        if (inputIndex == 0){
+            nand1.connect(0, emitter);
+        }else if (inputIndex == 1){
+            nand1.connect(1, emitter);
+        }else {nand3.connect(1, emitter);}
+    }
+}
